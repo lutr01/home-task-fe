f7a9e8b758676ea5be5648695ea89c16ba6702c9
Lu <lubova.tretjakova@gmail.com>
2020-02-26 15:23:04 +0100

    preparing to interact with the elements on the page

--- cypress/fixtures/pages/page.js
+++ cypress/fixtures/pages/page.js
@@ -1,6 +1,21 @@
 import pages from './pages.json'
 
 export const goToPage = pageName => {
-    cy.visit(pages[pageName])
+    const page = getPage(pageName)
+    cy.visit(page.path)
 }
+
+export const getPage = pageName => {
+	const page = pages[pageName]
+	return {
+        path: page.Path,
+		buttons: {
+			...page.Buttons,
+		},
+		fields: {
+			...page.Fields,
+		},
+	}
+}
+
 
